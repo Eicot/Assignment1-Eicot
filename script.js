f6f3675e27975506edb448d1d3abf01d69bc8b50
Eicot <zha232zha@gmail.com>
2022-12-28 09:38:01 +0000

    load geojson file into js

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 let singapore = [1.3521, 103.8198]; //Singapore latlong
-const map = L.map('map').setView(singapore, 12); //Setting center point
+let map = L.map('map').setView(singapore, 12); //Setting center point
 
-const mapLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
-});
-
-mapLayer.addTo(map);
+}).addTo(map);
 
 loadData();
 
-async function loadData(){
-    const response = await axios.get("sg.geojson");
 
-    const layer1 = L.geoJson(response.data, {
+async function loadData() {
+
+    let response = await axios.get("singapore-population-2022.geojson");
+
+
+    const singaporePopulation = L.geoJson(response.data, {
+
         onEachFeature: function(feature, layer) {
+
             console.log(feature);
 
-            layer.bindPopup(feature.coordinates);
+
+            layer.bindPopup(feature.properties.planningArea);
 
         }
-    }
-)}
+    })
 
 
-layer1.addTo(map);
-popLayer.setStyle({
-    color:'red'
-     
-})
+    singaporePopulation.addTo(map);
+
+    singaporePopulation.setStyle({
+        color: 'blue'
+    })
+}  
