4aec387d9a2ce7a6678967d1aaa248706cb91c0f
Eicot <zha232zha@gmail.com>
2023-01-01 12:53:27 +0000

    Added legend for color categories

--- script.js
+++ script.js
@@ -62,12 +62,37 @@ info.onAdd = function (layer) {
 
 // method that we will use to update the control based on feature properties passed
 info.update = function (props) {
-    this._div.innerHTML = '<h4>Singapore Population</h4>' +  (props ?
-        '<b>' + props.planningArea + '</b><br />' + props.totalPopulation + ' people '
-        : 'Hover over a state');
+    this._div.innerHTML = 
+    '<h4>Singapore Population</h4>'+ 
+    (props ?'<b>' + 
+    "Planning Area : " + props.planningArea + '</b><br />' + 
+    "Sub Zone : " + props.subZone + '</b><br />' + 
+    props.totalPopulation
+    : 'Hover over a state');
 };
 
 info.addTo(map);
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 10000, 20000, 30000, 40000, 50000],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+        // '<h4>Singapore Population</h4>'+ '</b><br />' + 
+            '<i style="background:' + colorPop(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? ' - ' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(map);
 
 
 loadData();
