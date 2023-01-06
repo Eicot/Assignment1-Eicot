39eccbc614903c67dd72f7c56fd5f37915c841b2
Eicot <zha232zha@gmail.com>
2023-01-06 15:08:03 +0000

    issue with data input

--- data.js
+++ data.js
@@ -1,16 +1,44 @@
 async function loadData() {
     const response = await axios.get("singaporePopulationTable.json");
-    // console.log(response)
+    console.log(response)
     return response.data;
 }
 
 loadData();
 
-function transformData(data) {
-    const population = data[1].map(function(dataPoint) {
+
+function transformData1(data) {
+    const popt = data.map(function(dataPoint) {
         return {
-            "amount" : dataPoint.unit,
-            "year": dataPoint.date
+            "amount" : dataPoint.populationTotal,
+            "year": dataPoint.year
+        }
+    })
+    console.log("population=", popt);
+
+    const seriesB = [];
+
+    for (let dataPoint of popt) {
+        const pop = dataPoint.amount;
+        const years = dataPoint.year;
+        console.log(pop);
+        console.log(years);
+
+        seriesB.push({
+            x: dataPoint.year,
+            y: dataPoint.amount
+        })
+    }
+    console.log(seriesB); 
+    return seriesB;  
+
+}
+
+function transformData(data) {
+    const population = data.map(function(dataPoint) {
+        return {
+            "amount" : dataPoint.populationFemale,
+            "year": dataPoint.year
         }
     })
     console.log("population=", population);
--- graph.js
+++ graph.js
@@ -1,70 +1,89 @@
-function drawchart1() {
-    const options =  {
+const options =  {
     chart: {
         type: 'line',
         height:"100%"
     },
     series:[
-        // NO DATA
+    
     ],
+   
     // what to show there is no data
     noData: {
         "text": "Loading..."
     }
-   
     
 }
-
+ 
+// create the chart
 const chart = new ApexCharts(document.querySelector('#chart1'), options);
-
+ 
 // render the chart
 chart.render()
-}
-drawchart1();
 
-function drawchart2() {
-    const options =  {
-    chart: {
-        type: 'line',
-        height:"100%"
-    },
-    series:[
-        // NO DATA
-    ],
-    // what to show there is no data
-    noData: {
-        "text": "Loading..."
-    }
+// when all the content are ready
+// the DOMContentLoaded for the window happens when
+// the DOM elements have been created
+// window.addEventListener("DOMContentLoaded", async function(){
+//     // the loadData function is from data.js
+//     const data = await loadData('data.js');
+//     const series1 = transformData(data);
+//     const seriesB = transformData1(data);
+
+//     // display the loaded data as a series in the chart
+//     chart.updateSeries([{
+//         'name':'Population',
+//         'data': series1
+//     },
+//     {
+//         'name':'Growth',
+//         'data': seriesB
+//     }])
+// })
+
+
+// function drawchart2() {
+//     const options =  {
+//     chart: {
+//         type: 'line',
+//         height:"100%"
+//     },
+//     series:[
+//         // NO DATA
+//     ],
+//     // what to show there is no data
+//     noData: {
+//         "text": "Loading..."
+//     }
    
     
-}
+// }
 
-const chart = new ApexCharts(document.querySelector('#chart2'), options);
+// const chart = new ApexCharts(document.querySelector('#chart2'), options);
 
-// render the chart
-chart.render()
-}
-drawchart2();
+// // render the chart
+// chart.render()
+// }
+// drawchart2();
 
-function drawpie1() {
-    const options =  {
-        chart: {
-            type: 'pie',
-            height:"280"
-        },
-        series:[ 100
-            // NO DATA
-        ],
-        // what to show there is no data
-        noData: {
-            "text": "Loading..."
-        }
+// function drawpie1() {
+//     const options =  {
+//         chart: {
+//             type: 'pie',
+//             height:"280"
+//         },
+//         series:[ 100
+//             // NO DATA
+//         ],
+//         // what to show there is no data
+//         noData: {
+//             "text": "Loading..."
+//         }
        
         
-    }
+//     }
 
-const chart = new ApexCharts(document.querySelector('#pie1'), options);
-// render the chart
-chart.render()
-}
-drawpie1();
+// const chart = new ApexCharts(document.querySelector('#pie1'), options);
+// // render the chart
+// chart.render()
+// }
+// drawpie1();
