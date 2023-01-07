127aa46935d5e60f0e998e5cc3ca075f1421673f
Replit user <>
2023-01-07 03:07:32 +0000

    data update

--- data.js
+++ data.js
@@ -1,63 +1,88 @@
 async function loadData() {
-    const response = await axios.get("singaporePopulationTable.json");
-    console.log(response)
-    return response.data;
+  const response = await axios.get("singaporePopulation.json");
+  // console.log(response)
+  return response.data;
 }
 
 loadData();
 
-
 function transformData1(data) {
-    const popt = data.map(function(dataPoint) {
-        return {
-            "amount" : dataPoint.populationTotal,
-            "year": dataPoint.year
-        }
-    })
-    console.log("population=", popt);
-
-    const seriesB = [];
-
-    for (let dataPoint of popt) {
-        const pop = dataPoint.amount;
-        const years = dataPoint.year;
-        console.log(pop);
-        console.log(years);
-
-        seriesB.push({
-            x: dataPoint.year,
-            y: dataPoint.amount
-        })
+  const population = data.map(function(dataPoint) {
+    return {
+      "pop": dataPoint.populationTotal,
+      "year": dataPoint.year
     }
-    console.log(seriesB); 
-    return seriesB;  
+  })
+  // console.log("population=", popt);
+
+  const series1 = [];
+
+  for (let dataPoint of population) {
+    const pop = dataPoint.pop;
+    const years = dataPoint.year;
+    // console.log(pop);
+    // console.log(years);
+
+    series1.push({
+      x: dataPoint.year,
+      y: dataPoint.pop
+    })
+  }
+  // console.log(seriesB);
+  return series1;
 
 }
 
-function transformData(data) {
-    const population = data.map(function(dataPoint) {
-        return {
-            "amount" : dataPoint.populationFemale,
-            "year": dataPoint.year
-        }
-    })
-    console.log("population=", population);
-
-    const series1 = [];
-
-    for (let dataPoint of population) {
-        const pop = dataPoint.amount;
-        const years = dataPoint.year;
-        // console.log(pop);
-        // console.log(years);
-
-        series1.push({
-            x: dataPoint.year,
-            y: dataPoint.amount
-        })
+function transformData2(data) {
+  const populationFemale = data.map(function(dataPoint) {
+    return {
+      "pop": dataPoint.populationFemale,
+      "year": dataPoint.year
     }
-    console.log(series1); 
-    return series1;
-  
+  })
+  console.log("population=", populationFemale);
+
+  const series2 = [];
+
+  for (let dataPoint of populationFemale) {
+    const pop = dataPoint.pop;
+    const years = dataPoint.year;
+    // console.log(pop);
+    // console.log(years);
+
+    series2.push({
+      x: dataPoint.year,
+      y: dataPoint.pop
+    })
+  }
+  // console.log(series2);
+  return series2;
+
+}
+
+function transformData3(data) {
+  const populationMale = data.map(function(dataPoint) {
+    return {
+      "pop": dataPoint.populationMale,
+      "year": dataPoint.year
+    }
+  })
+  // console.log("population=", populationMale);
+
+  const series3 = [];
+
+  for (let dataPoint of populationMale) {
+    const pop = dataPoint.pop;
+    const years = dataPoint.year;
+    // console.log(pop);
+    // console.log(years);
+
+    series3.push({
+      x: dataPoint.year,
+      y: dataPoint.pop
+    })
+  }
+  // console.log(series3);
+  return series3;
 
 }
--- graph.js
+++ graph.js
@@ -1,69 +1,122 @@
-const options =  {
-    chart: {
+window.addEventListener("DOMContentLoaded", async function() {
+  // the loadData function is from data.js
+  const data = await loadData('data.js');
+  const series1 = transformData1(data);
+  const series2 = transformData2(data);
+  const series3 = transformData3(data)
+
+  function drawchart1() {
+    const options = {
+      chart: {
         type: 'line',
-        height:"100%"
-    },
-    series:[
-    
-    ],
-   
-    // what to show there is no data
-    noData: {
+        height: "100%"
+      },
+      series: [
+
+      ],
+
+      // what to show there is no data
+      noData: {
         "text": "Loading..."
+      }
+
     }
-    
-}
- 
-// create the chart
-const chart = new ApexCharts(document.querySelector('#chart1'), options);
- 
-// render the chart
-chart.render()
 
-// when all the content are ready
-// the DOMContentLoaded for the window happens when
-// the DOM elements have been created
-// window.addEventListener("DOMContentLoaded", async function(){
-//     // the loadData function is from data.js
-//     const data = await loadData('data.js');
-//     const series1 = transformData(data);
-//     const seriesB = transformData1(data);
+    // create the chart
+    const chart = new ApexCharts(document.querySelector('#chart1'), options);
 
-//     // display the loaded data as a series in the chart
-//     chart.updateSeries([{
-//         'name':'Population',
-//         'data': series1
-//     },
-//     {
-//         'name':'Growth',
-//         'data': seriesB
-//     }])
-// })
+    // render the chart
+    chart.render()
 
 
-// function drawchart2() {
-//     const options =  {
-//     chart: {
-//         type: 'line',
-//         height:"100%"
-//     },
-//     series:[
-//         // NO DATA
-//     ],
-//     // what to show there is no data
-//     noData: {
-//         "text": "Loading..."
-//     }
-   
-    
-// }
+    // display the loaded data as a series in the chart
+    chart.updateSeries([{
+      'name': 'Population',
+      'data': series1
+    },
+    {
+      'name': 'Female Population',
+      'data': series2
+    },
+    {
+      'name': 'Male Population',
+      'data': series3
+    },
 
-// const chart = new ApexCharts(document.querySelector('#chart2'), options);
+    ])
 
-// // render the chart
-// chart.render()
-// }
-// drawchart2();
+  }
+  drawchart1();
+
+  function drawchart2() {
+    const options = {
+      chart: {
+        type: 'line',
+        height: "100%"
+      },
+      series: [
+
+      ],
+
+      // what to show there is no data
+      noData: {
+        "text": "Loading..."
+      }
+
+    }
+
+    // create the chart
+    const chart = new ApexCharts(document.querySelector('#chart2'), options);
+
+    // render the chart
+    chart.render()
+
+
+    // display the loaded data as a series in the chart
+    chart.updateSeries([{
+      'name': 'Population',
+      'data': series1
+    },
+    {
+      'name': 'Female Population',
+      'data': series2
+    },
+    {
+      'name': 'Male Population',
+      'data': series3
+    },
+
+    ])
+
+  }
+  drawchart2();
+
+  function drawpie1() {
+    const options = {
+      chart: {
+        type: 'pie',
+        height: "280"
+      },
+      series: [100
+        // NO DATA
+      ],
+      // what to show there is no data
+      noData: {
+        "text": "Loading..."
+      }
+
+
+    }
+
+    const chart = new ApexCharts(document.querySelector('#pie1'), options);
+    // render the chart
+    chart.render()
+  }
+  drawpie1();
+})
+
+
+
 
 // function drawpie1() {
 //     const options =  {
@@ -78,8 +131,8 @@ chart.render()
 //         noData: {
 //             "text": "Loading..."
 //         }
-       
-        
+
+
 //     }
 
 // const chart = new ApexCharts(document.querySelector('#pie1'), options);
